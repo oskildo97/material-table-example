27723c8e0f4f541984825fd92060a2df58c1a4d4
oskildo <podolopezospina@gmail.com>
2024-01-22 13:46:41 -0500

    tabla con 3 campos y data dummy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import {MatTableModule} from '@angular/material/table';
+import { User } from './interfaces/user';
+
+
+const CUSTOMER_DATA: User[] = [
+  { name: 'oskildo', lasName: 'podols', age: 20 }
+];
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatTableModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'tabla2';
+  displayedColumns: string[] = [ 'name', 'lasName', 'age' ];
+  dataSource = CUSTOMER_DATA;
 }
